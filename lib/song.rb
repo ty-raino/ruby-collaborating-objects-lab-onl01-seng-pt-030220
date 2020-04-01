9ab434940b25b4a5cf13c0077ab4250746fa0852
Ty Raino <mstymetris@yahoo.com>
2020-04-01 05:04:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,8 +15,8 @@ class Song
     end
 
     def self.new_by_filename(file)
-      song = file.split(" - ")[1]
-      artist = file.split(" - ")[0]
+      # song = file.split(" - ")[1]
+      # artist = file.split(" - ")[0]
 
       new_song = self.new(song)
       #new_song.artist_name = artist
