8ff04352d386f6448bf0699913e78d3778a2195a
Ty Raino <mstymetris@yahoo.com>
2020-04-01 03:23:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,8 +13,7 @@ class Song
     end
 
     def self.new_by_filename
-      new = Song.new_by_filename(name).each do |n|
-        n.name
+      new = Song.new_by_filename(name) * 4
       end
     end
 
