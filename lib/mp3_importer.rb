8e3063c73db4da1bee42687c4764ea7db7b8aa79
Ty Raino <mstymetris@yahoo.com>
2020-04-01 02:09:22 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,22 @@
+
+class MP3Importer
+
+  attr_accessor :path
+
+    def initialize(path)
+        @path = path
+    end
+
+    def files
+      binding.pry
+      Dir.glob("#{@path}/*.mp3").map do |file|
+        file.gsub("./spec/fixtures/mp3s/", "")
+      end
+    end
+
+    def import
+
+    end
+
+
+end
